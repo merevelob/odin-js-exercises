cc3a0828574aae399325dc644a5dbc054298ada7
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-19 16:44:17 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    reversedStrArr = str.split('').reverse();
+    return reversedStrArr.join('');
 };
 
 // Do not edit below this line
