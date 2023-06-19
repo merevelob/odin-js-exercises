792fa386e4070a3e7496e2527e5a4d249cd97910
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-19 16:29:18 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
+    
+    let repeatedStr = '';
+    
+    for (let i = 1; i <= num; i++) {
+        repeatedStr += str;     
+    }
+    
+    return repeatedStr;
 };
 
 // Do not edit below this line
