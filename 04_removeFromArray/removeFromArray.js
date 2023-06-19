252725539a8a56804b1b2a4e8b50f50911dd0af1
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-19 18:27:34 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let filteredArr = [...arr];
+    for (let i = 1; i < arguments.length; i++) {
+        filteredArr = filteredArr.filter(ele => ele !== arguments[i]);
+    }
+    return filteredArr;
 };
 
 // Do not edit below this line
