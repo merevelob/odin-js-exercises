2ce35f14f47f4a1ff71a417ac4de5450f33938f3
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-19 19:21:52 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    const range = [num1, num2].sort((a, b) => a - b);
+    let sum = 0;
+    for (let i = range[0]; i <= range[1]; i++) {
+        sum += i;  
+    }
+    return sum;
 };
 
 // Do not edit below this line
