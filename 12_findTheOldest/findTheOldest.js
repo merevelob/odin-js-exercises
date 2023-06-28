77698866de385c8ba2267d4b39c1d5285fc23e5c
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-28 18:19:07 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(person) {
+        let currentYear = new Date().getFullYear();
+        return person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (currentYear - person.yearOfBirth)
+    }
 
+    const sorted = people.sort((a, b) => getAge(b) - getAge(a));
+    return sorted[0];
 };
 
 // Do not edit below this line
